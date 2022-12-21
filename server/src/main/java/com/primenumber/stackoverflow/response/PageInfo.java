9227f742e6929dbaac357870d53d055a72bf9600
tami0010 <ggoct101@naver.com>
2022-12-21 10:49:56 +0900

    feat: 기본 응답 dto 구현

---
+++ server/src/main/java/com/primenumber/stackoverflow/response/PageInfo.java
@@ -0,0 +1,13 @@
+package com.primenumber.stackoverflow.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PageInfo {
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+}
