75911203b2dd557d8f23544d05a243270f3c430b
syk506 <suuunyy0@naver.com>
2022-12-22 21:18:52 +0900

    fix: 질문 작성 레이아웃 수정

--- client/src/pages/AskQuestion.js
+++ client/src/pages/AskQuestion.js
@@ -3,8 +3,7 @@ import AskQuestionForm from '../components/AskQuestionForm';
 
 const MainContainer = styled.div`
   width: 100%;
-  max-width: var(--max-width);  
-  /* background: none; */
+  max-width: var(--max-width);
   display: flex;
   justify-content: space-between;
   margin: 0 auto;
@@ -14,10 +13,14 @@ const MainContainer = styled.div`
 const MainContent = styled.main`
   max-width: calc(var(--max-width) - var(--nav-width));
   width: calc(100% - var(--nav-width));
-  padding: var(--main-outline-margin);
+  padding: 0 var(--main-outline-margin);
+  background-image: url("https://cdn.sstatic.net/Img/ask/background.svg?v=2e9a8205b368");
+  background-repeat: no-repeat;
+  background-position: right top;
+  background-size: 550px;
+
   h2 {
-    color: var(--font-color-title);
-    padding-bottom: 70px;
+    color: var(--font-color-title);  
   }
 
   h1 {
@@ -25,20 +28,51 @@ const MainContent = styled.main`
   }
 
   .ask-question-info {
+    color: #374F51;
     background-color: #EBF4FB;
     padding: 24px;
     border-radius: 5px;
-    border: 1px solid black;
+    border: 1px solid #BBD9F1;
+    margin-top: 40px;
+
+    > h2 {
+      font-weight: 400;
+      color: #374F51;
+      margin-bottom: 8px;
+    }
+
+    .second-info {
+      margin-bottom: 15px;
+    }
+
+    > h5 {
+      color: #374F51;
+      margin-bottom: 8px;
+    }
+  }
+  
+  li {
+    list-style-position: inside;
+    margin-left: 15px;
+    font-size: 13px;
   }
 `;
 
-/*
-const Title = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
+const AskQuestionContainer = styled.div`
+  width: 850px;
 `;
-*/
+
+const AskQuestionDescription = styled.div`
+  .ask-question-title {
+    height: 86px;
+    margin-top: 20px;
+
+    h2 {
+      font-size: 27px;
+      line-height: 86px;
+    }
+  }
+`;
 
 function AskQuestion() {
   return (
