90f2914719581f76e4a66c620c9124fe46ded99a
sGOM <pooh6195@naver.com>
2022-12-27 05:14:04 +0900

    refactor: import optimize

--- server/src/main/java/com/primenumber/stackoverflow/auth/JwtTokenizer.java
+++ server/src/main/java/com/primenumber/stackoverflow/auth/JwtTokenizer.java
@@ -7,7 +7,6 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
