b5aab1ed3f685178d3f31d562ca6b246a2d51dd7
sGOM <pooh6195@naver.com>
2022-12-26 16:35:24 +0900

    feat: 인증 관련 Handler
    인증 성공, 실패 시 반환 값을 지정하는 handler 정의

---
+++ server/src/main/java/com/primenumber/stackoverflow/auth/handler/MemberAuthenticationFailureHandler.java
@@ -0,0 +1,21 @@
+package com.primenumber.stackoverflow.auth.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class MemberAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        log.error("# Authentication failed: {}", exception.getMessage());;
+    }
+}
