0c145c1fd2febc5a833577cdeb7256e51e9025ce
sGOM <pooh6195@naver.com>
2022-12-26 14:19:23 +0900

    feat: Member Service 및 관련 Dto 구현
    `Member` 관련 CRUD 구현 (이후 예외 처리 등의 검사 추가 필요)
    
    이후 Spring Security 추가 시에 사용할 `UserDetails` 구현 클래스 `MemberPrincipal` 구현
    
    CRUD 및 Response 에 사용할 DTO 구현

---
+++ server/src/main/java/com/primenumber/stackoverflow/dto/MemberDto.java
@@ -0,0 +1,69 @@
+package com.primenumber.stackoverflow.dto;
+
+import com.primenumber.stackoverflow.entity.Member;
+import com.primenumber.stackoverflow.entity.util.MemberStatus;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+public class MemberDto {
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class Post {
+        @NotBlank
+        private String email;
+        @NotBlank
+        private String password;
+
+        private String displayName;
+
+        public static MemberDto.Post of(String email, String password, String displayName) {
+            return new Post(email, password, displayName);
+        }
+
+        public static Member toEntity(MemberDto.Post dto) {
+            return Member.of(dto.email, dto.password, dto.displayName);
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class Patch {
+        private String password;
+
+        private String displayName;
+
+        public static MemberDto.Patch of(String password, String displayName) {
+            return new Patch(password, displayName);
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class Response {
+        private Long id;
+        private String email;   // 표시할 필요가 없다면 삭제 할 수 있음
+        private String displayName;
+        private MemberStatus status;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
+
+        public static MemberDto.Response of(Long id, String email, String displayName, MemberStatus status, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+            return new Response(id, email, displayName, status, createdAt, modifiedAt);
+        }
+
+        public static MemberDto.Response from(Member entity) {
+            return new Response(
+                    entity.getId(),
+                    entity.getEmail(),
+                    entity.getDisplayName(),
+                    entity.getStatus(),
+                    entity.getCreatedAt(),
+                    entity.getModifiedAt()
+            );
+        }
+    }
+}
