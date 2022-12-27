fe28d8098e3ccaf93d5c947c416a4f171696c611
tami0010 <ggoct101@naver.com>
2022-12-27 18:21:07 +0900

    chore: StackOverflowApplication.java 애너테이션 추가

--- server/src/main/java/com/primenumber/stackoverflow/StackOverflowApplication.java
+++ server/src/main/java/com/primenumber/stackoverflow/StackOverflowApplication.java
@@ -2,8 +2,10 @@ package com.primenumber.stackoverflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StackOverflowApplication {
 
 	public static void main(String[] args) {
