24381c2d0c4386517fdd6d3681c6e960107dfe54
gidoldory <gidol79@gmail.com>
2022-12-28 11:30:25 +0900

    feat: 질문목록, 답변 crud 기능 추가

--- client/src/components/answers/EditForm.js
+++ client/src/components/answers/EditForm.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 import { BasicButton, CancelButton } from '../Button';
+import Loading from '../Loading';
+import { ANSWER_ENDPOINT } from '../../api/Answer';
+import useFetch from '../../hooks/useFetch';
+import { useNavigate, useParams } from 'react-router-dom';
+import { answerUpdate } from '../../api/Answer';
 
 const AnswerEditForm = styled.form`
-	margin-bottom: 10px;
+	margin: 20px 0 10px;
 
 	h3 {
 		margin-bottom: 24px;
+		font-weight: lighter;
 	}
 	textarea {
 		padding: 10px;
@@ -21,7 +27,7 @@ const AnswerEditForm = styled.form`
 			outline: var(--outline-input-focus);
 		}
 	}
-	.explain_02 {
+	.explain-02 {
 		margin-bottom: 10px;
 		p {
 			font-size: var(--font-size-0-9rem);
@@ -31,51 +37,80 @@ const AnswerEditForm = styled.form`
 `;
 
 const EditForm = ({ item }) => {
-	const defaultValue =
-		'I\'d say it\'s not supported. https://fullcalendar.io/docs/selection/select_callback/ indicates that when a selection is made, the callback will return a single "resource" object which will indicate the resource chosen by the user. This implies that selecting multiple resources\n\n via dragging the mouse on the timeline is not possible.\n';
+	const { questionId, answerId } = useParams();
+	const navigate = useNavigate();
+	const [answerItem, isLoading, error] = useFetch(
+		ANSWER_ENDPOINT(questionId) + '/' + answerId
+	);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		alert('submit');
+		const form = e.target;
+
+		const successCb = () => (window.location.href = `/questions/${questionId}`);
+		const failCb = () => alert('답변 수정에 실패했습니다.');
+		answerUpdate(
+			{
+				memberId: 1,
+				answerId: Number(answerId),
+				questionId: Number(questionId),
+				content: form.answer_text.value,
+				updateAt: new Date(),
+			},
+			successCb,
+			failCb
+		);
 	};
 	return (
 		<AnswerEditForm onSubmit={handleSubmit}>
-			<h3>Answer</h3>
-			<textarea name="answer_text" defaultValue={defaultValue} />
-			<div className="explain_02">
-				<p>
-					I'd say it's not supported.{' '}
-					<a href="https://fullcalendar.io/docs/selection/select_callback/">
-						https://fullcalendar.io/docs/selection/select_callback/
-					</a>{' '}
-					indicates that when a selection is made, the callback will return a
-					single "resource" object which will indicate the resource chosen by
-					the user. This implies that selecting multiple resources via dragging
-					the mouse on the timeline is not possible.
-				</p>
-				<p>
-					You can create events via the "events" feeds which are associated with
-					multiple resources (see{' '}
-					<a href="https://fullcalendar.io/docs/resource_data/associating_events/">
-						https://fullcalendar.io/docs/resource_data/associating_events/
-					</a>
-					) but it's not supported via the UI yet.
-				</p>
-				<p>
-					You'd have to create your own UI to allow the user to specify this,
-					and/or put in a feature request to the maintainer to add this support
-					in future (it may already be on the roadmap, I haven't looked).
-				</p>
-			</div>
-			<div>
-				<BasicButton type="submit" height="40">
-					Save edits
-				</BasicButton>
-				&nbsp;
-				<CancelButton type="button" height="40">
-					Cancel
-				</CancelButton>
-			</div>
+			{error && <div>답변 정보 조회 실패</div>}
+			{isLoading ? (
+				<Loading />
+			) : (
+				<>
+					<h3>Answer</h3>
+					<textarea name="answer_text" defaultValue={answerItem[0].content} />
+					<div className="explain-02">
+						<p>
+							I'd say it's not supported.{' '}
+							<a href="https://fullcalendar.io/docs/selection/select_callback/">
+								https://fullcalendar.io/docs/selection/select_callback/
+							</a>{' '}
+							indicates that when a selection is made, the callback will return
+							a single "resource" object which will indicate the resource chosen
+							by the user. This implies that selecting multiple resources via
+							dragging the mouse on the timeline is not possible.
+						</p>
+						<p>
+							You can create events via the "events" feeds which are associated
+							with multiple resources (see{' '}
+							<a href="https://fullcalendar.io/docs/resource_data/associating_events/">
+								https://fullcalendar.io/docs/resource_data/associating_events/
+							</a>
+							) but it's not supported via the UI yet.
+						</p>
+						<p>
+							You'd have to create your own UI to allow the user to specify
+							this, and/or put in a feature request to the maintainer to add
+							this support in future (it may already be on the roadmap, I
+							haven't looked).
+						</p>
+					</div>
+					<div>
+						<BasicButton type="submit" height="40">
+							Save edits
+						</BasicButton>
+						&nbsp;
+						<CancelButton
+							type="button"
+							height="40"
+							onClick={() => navigate(`/questions/${questionId}`)}
+						>
+							Cancel
+						</CancelButton>
+					</div>
+				</>
+			)}
 		</AnswerEditForm>
 	);
 };
--- client/src/util/common.js
+++ client/src/util/common.js
@@ -1,12 +1,12 @@
 // 천 단위 콤마 구분(10000 => 10,000)
-export const AmountComma1000 = (number) => {
-	return Number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+export const AmountDisplay = (number) => {
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
 // 현재 시간 기준 날짜 변환(1 min ago, 2 mins ago, 1 sec ago)
 export const DateConvert = (createAt) => {
-	const milliSeconds = new Date() - createAt;
-
+	let cDate = new Date(createAt);
+	const milliSeconds = new Date() - cDate;
 	const seconds = milliSeconds / 1000;
 	if (seconds < 60) return `${Math.floor(seconds)} secs ago`;
 
@@ -22,9 +22,31 @@ export const DateConvert = (createAt) => {
 	const weeks = days / 7;
 	if (weeks < 5) return `${Math.floor(weeks)} weeks ago`;
 
-	const months = days / 30;
+	const lastDayOfMonth = new Date(
+		cDate.getFullYear(),
+		cDate.getMonth() + 1,
+		0
+	).getDate();
+	const months = days / lastDayOfMonth;
 	if (months < 12) return `${Math.floor(months)} months ago`;
 
 	const years = days / 365;
 	return `${Math.floor(years)} years ago`;
+};
+
+// 날짜 표시 포맷(Jun 8, 2011 at 2:43)
+export const DateFormat = (createAt) => {
+	const strDate = new Date(createAt).toLocaleString('en-US', {
+		month: 'short',
+		day: 'numeric',
+		year: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+	});
+	const arrDate = strDate.split(',');
+	const time = arrDate[2].trim().split(' '); // '5:02 PM'
+	let [hour, minute] = time[0].split(':');
+	if (time[1] === 'PM') hour = Number(hour) + 12;
+
+	return `${arrDate[0]}, ${arrDate[1]} at ${hour}:${minute}`;
 };
