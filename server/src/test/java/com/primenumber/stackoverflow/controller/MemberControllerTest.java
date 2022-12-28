c0e9cebd97f0bd807b4ee7d39bb49f07d10b0fa4
Heo SeokJin <pooh6195@naver.com>
2022-12-28 10:22:51 +0900

    feat: Member Controller 테스트를 완성하고 API Spec 문서 작성 (#35)
    * feat: Member 엔티티 연관관계 추가
    
    추가적으로  이후에 유저 정보 수정을 통해 수정할 수 있는 필드는 `@Setter` 추가
    
    * feat: Member Repository 구현
    
    로그인 시에 Email 을 통해서 사용자를 특정하기 때문에 `findByEmail` 추가
    
    * feat: Member Service 및 관련 Dto 구현
    
    `Member` 관련 CRUD 구현 (이후 예외 처리 등의 검사 추가 필요)
    
    이후 Spring Security 추가 시에 사용할 `UserDetails` 구현 클래스 `MemberPrincipal` 구현
    
    CRUD 및 Response 에 사용할 DTO 구현
    
    * feat: `MemberController` 구현
    
    * refactor: build.gradle 파일 리팩토링
    
    * feat: JWT 를 이용한 AuthenticationFilter 구현
    
    `email`과 `password`를 통해 인증을 시도
    
    인증 성공 시 `response.header`에 JWT 추가
    
    * feat: JWT 를 통한 검증 필터 구현
    
    `request.header`에 JWT 를 통해 권한을 확인
    
    * feat: 인증 관련 Handler
    
    인증 성공, 실패 시 반환 값을 지정하는 handler 정의
    
    * feat: Login 관련 DTO 정의
    
    로그인에 필요한 데이터를 전달해줄 DTO 정의
    
    * feat: JWT 유틸 클래스 정의
    
    이후 secret을 환경변수로 변경해줄 필요가 있음
    
    * feat: Security 설정
    
    앞서 정의한 필터들을 이용한 SecurityFilter 설정
    
    * feat: Security 관련 Test 정의
    
    `Tokenizer` 에 대한 테스트 코드와
    `login`, `logout` 에 대한 테스트 코드 작성
    
    * feat: Member 관련 테스트 코드 작성
    
    * feat: Spring-Rest-Docs 사용을 위한 adoc 파일 생성
    
    이후 추가 작성
    
    * fix: JWT 검증 필터가 Principal 을 Authentication 로 등록
    
    `AuthenticationToken` 을 생성할 때 `MemberPrincipal` 을 사용하고
    이를 `SecurityContextHolder` 에 등록해 `@AuthenticationPrincipal` 에 주입 될 수 있도록함
    
    `username` -> `email` 로 통일
    
    * fix: `MemberPrincipal`에 `id` 추가 및 비밀번호 암호화
    
    `MemberPrincipal` 에 `id` 필드를 추가해 pk 로 객체를 비교할 수 있게함
    
    Member Patch, Post 시에 `password` 를 암호화
    
    * refactor: 변수명 변경
    
    `username` -> `email` 로 통일
    
    * fix: 회원 목록 조회의 `Pageable` 기본 값 수정
    
    Front 에서 보내온 `page` 는 1부터 시작하므로 1을 default로 한다
    
    * fix: JPA Auditing 추가
    
    JPA 저장 시에 자동 생성되어야 하는 Auditing Field 를 위해 설정 추가
    
    이후 `JpaConfig` 등을 생성해서 옮기는 것이 좋아보임
    
    * refactor: `@EnableJpaAuditing`을 `JpaConfig` 로 이동
    
    의미적으로 JpaConfig 가 Auditing 설정을 가지는게 좋고
    
    Repository 를 건드리는 테스트 실행 시에 SpringApplication 을 전부 로드하지 않을 수 있음
    
    * fix: 암호화 추가에 따른 테스트 코드 수정
    
    * fix: 회원 목록 조회에 Page 관련 로직 수정에 대한 테스트 코드 수정
    
    * refactor: 로그인이 문서 위에 오도록 리팩토링
    
    * refactor: import optimize
    
    * feat: API Spec 문서 작성을 위한 asciidoctor 의존성 설정
    
    버전 변경 및 정적 파일 생성
    
    * feat: 이전 테스트들을 수정하고 `MemberController` 테스트 완성
    
    추가된 테스트
    - [GET] 단일 회원 정보 조회
    - [PATCH] 회원 정보 수정
    - [DELETE] 회원 정보 삭제
    
    * feat: 잘못된 부분 수정하고 새로 추가한 테스트에 대한 문서 작성
    
    추가된 테스트
    - [GET] 단일 회원 정보 조회
    - [PATCH] 회원 정보 수정
    - [DELETE] 회원 정보 삭제

--- server/src/test/java/com/primenumber/stackoverflow/controller/MemberControllerTest.java
+++ server/src/test/java/com/primenumber/stackoverflow/controller/MemberControllerTest.java
@@ -1,8 +1,8 @@
 package com.primenumber.stackoverflow.controller;
 
 import com.google.gson.Gson;
-import com.primenumber.stackoverflow.dto.LoginDto;
 import com.primenumber.stackoverflow.dto.MemberDto;
+import com.primenumber.stackoverflow.dto.security.MemberPrincipal;
 import com.primenumber.stackoverflow.entity.Member;
 import com.primenumber.stackoverflow.service.MemberService;
 import config.TestSecurityConfig;
@@ -13,7 +13,10 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,13 +26,14 @@ import java.util.List;
 import static com.primenumber.stackoverflow.util.Stub.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("컨트롤러 - 회원")
 @AutoConfigureRestDocs
@@ -38,18 +42,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MemberControllerTest {
 
     @Autowired private MockMvc mvc;
-
     @MockBean private MemberService memberService;
 
     @DisplayName("[GET] 회원 페이지 - 정상 호출")
     @Test
-    public void givenNothing_whenQuestingMemberPage_thenReturnMemberPage() throws Exception {
+    public void givenPageableParameter_whenQuestingMemberPage_thenReturnMemberPage() throws Exception {
         // Given with Stubbing
         Pageable pageable = PageRequest.of(0, 10, Sort.Direction.DESC, "createdAt");
         Page<Member> memberPage = createMemberPage(pageable);
 
-        given(memberService.searchMembers(any(Pageable.class)))
-                .willReturn(memberPage);
+        given(memberService.searchMembers(any(Pageable.class))).willReturn(memberPage);
 
         // When & Then
         mvc.perform(get("/members")
@@ -84,6 +86,40 @@ class MemberControllerTest {
                                 )
                         )
                 ));
+        then(memberService).should().searchMembers(any(Pageable.class));
+    }
+
+    @DisplayName("[GET] 단일 회원 정보 조회 - 정상 호출")
+    @Test
+    public void givenMemberId_whenQuestingMemberInfo_thenReturnMemberInfo() throws Exception {
+        // Given with Stubbing
+        Member member = createMember();
+
+        given(memberService.searchMember(anyLong())).willReturn(member);
+
+        // When & Then
+        mvc.perform(get("/members/{memberId}", member.getId()))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("get-member",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("memberId").description("회원의 PK")
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("response").type(JsonFieldType.OBJECT).description("결과 데이터"),
+                                        fieldWithPath("response.id").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                        fieldWithPath("response.email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("response.displayName").type(JsonFieldType.STRING).description("닉네임"),
+                                        fieldWithPath("response.status").type(JsonFieldType.STRING).description("회원 상태: 활동 상태 / 탈퇴 상태"),
+                                        fieldWithPath("response.createdAt").type(JsonFieldType.STRING).description("회원 등록일"),
+                                        fieldWithPath("response.modifiedAt").type(JsonFieldType.STRING).description("회원 정보 수정일")
+                                )
+                        )
+                ));
+        then(memberService).should().searchMember(anyLong());
     }
 
     @DisplayName("[POST] 회원 가입 - 정상 호출")
@@ -103,7 +139,7 @@ class MemberControllerTest {
                         .content(requestJson)
                 )
                 .andExpect(status().isCreated())
-                .andDo(document("post-members",
+                .andDo(document("post-member",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
                         requestFields(
@@ -114,5 +150,80 @@ class MemberControllerTest {
                                 )
                         )
                 ));
+        then(memberService).should().saveMember(any(MemberDto.Post.class));
+    }
+
+    @DisplayName("[PATCH] 회원 정보 수정 - 정상 호출")
+    @Test
+    public void givenMemberIdAndPatchInfo_whenUpdatingMemberInfo_thenReturnNothing() throws Exception {
+        // Given with Stubbing
+        Member member = createMember();
+        MemberDto.Patch dto = createMemberPatchDto();
+
+        Gson gson = new Gson();
+
+        String requestJson = gson.toJson(dto);
+        String jwt = createJwt(member);
+
+        willDoNothing().given(memberService).updateMember(anyLong(), any(MemberPrincipal.class), any(MemberDto.Patch.class));
+        given(memberService.searchMember(anyString())).willReturn(member);
+
+        // When & Then
+        mvc.perform(patch("/members/{memberId}", member.getId())
+                        .header(getJwtHeader(), getJwtPrefix() + " " + jwt)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson)
+                )
+                .andExpect(status().isOk())
+                .andDo(document("patch-member",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("memberId").description("회원의 PK")
+                        ),
+                        requestHeaders(
+                                headerWithName("Authorization").description("Access Token")
+                        ),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호").optional(),
+                                        fieldWithPath("displayName").type(JsonFieldType.STRING).description("닉네임").optional()
+                                )
+                        )
+                ));
+        then(memberService).should().updateMember(anyLong(), any(MemberPrincipal.class), any(MemberDto.Patch.class));
+        then(memberService).should().searchMember(anyString());
+    }
+
+    @DisplayName("[DELETE] 회원 정보 삭제 - 정상 호출")
+    @Test
+    public void givenMemberId_whenDeletingMember_thenReturnNothing() throws Exception {
+        // Given with Stubbing
+        Member member = createMember();
+
+        String jwt = createJwt(member);
+
+        willDoNothing().given(memberService).deleteMember(anyLong(), any(MemberPrincipal.class));
+        given(memberService.searchMember(anyString())).willReturn(member);
+
+        // When & Then
+        mvc.perform(delete("/members/{memberId}", member.getId())
+                        .header(getJwtHeader(), getJwtPrefix() + " " + jwt)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isNoContent())
+                .andDo(document("delete-member",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("memberId").description("회원의 PK")
+                        ),
+                        requestHeaders(
+                                headerWithName("Authorization").description("Access Token")
+                        )
+                ));
+        then(memberService).should().deleteMember(anyLong(), any(MemberPrincipal.class));
+        then(memberService).should().searchMember(anyString());
     }
 }
