1ab8de4da486549966fd0288204a5da87359b88b
sGOM <pooh6195@naver.com>
2022-12-30 00:47:52 +0900

    feat: 질문 CRUD 를 위한 Service 작성
    AnswerService 는 메서드명 변경으로 인해 수정 되었음

--- server/src/main/java/com/primenumber/stackoverflow/service/QuestionService.java
+++ server/src/main/java/com/primenumber/stackoverflow/service/QuestionService.java
@@ -1,22 +1,112 @@
 package com.primenumber.stackoverflow.service;
 
+import com.primenumber.stackoverflow.dto.QuestionDto;
+import com.primenumber.stackoverflow.dto.security.MemberPrincipal;
+import com.primenumber.stackoverflow.entity.Member;
 import com.primenumber.stackoverflow.entity.Question;
+import com.primenumber.stackoverflow.entity.QuestionTag;
+import com.primenumber.stackoverflow.entity.Tag;
 import com.primenumber.stackoverflow.entity.util.BasicStatus;
+import com.primenumber.stackoverflow.entity.util.MemberStatus;
+import com.primenumber.stackoverflow.exception.BusinessLogicException;
+import com.primenumber.stackoverflow.exception.ExceptionCode;
 import com.primenumber.stackoverflow.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.*;
+import java.util.stream.Collectors;
 
-@Slf4j
-@Service
 @RequiredArgsConstructor
+@Transactional
+@Service
 public class QuestionService {
+    // TODO: Status 가 삭제 상태인 것들에 대한 Action 생각하고 수정하기 (CRUD 모두)
     private final QuestionRepository questionRepository;
+    private final QuestionTagService questionTagService;
+    private final TagService tagService;
 
-    public Question findQuestion(long questionId) {
+    @Transactional(readOnly = true)
+    public Page<Question> searchQuestions(Pageable pageable) { return questionRepository.findAll(pageable); }
+
+    @Transactional(readOnly = true)
+    public Question searchQuestion(Long questionId) {
         return questionRepository.findByIdAndStatus(questionId, BasicStatus.ACTIVE)
-                .orElseThrow(() -> new EntityNotFoundException("해당 질문을 찾을 수 없습니다."));
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    public void saveQuestion(MemberPrincipal memberPrincipal, QuestionDto.Post dto) {
+        if (memberPrincipal.getId() == null) { throw new BusinessLogicException(ExceptionCode.UNAUTHORIZED_MEMBER); }
+
+        Question question = questionRepository.save(dto.toEntity(memberPrincipal.toEntity()));
+        List<Tag> tags = tagService.convertToTags(dto.getTags());
+
+        List<QuestionTag> questionTags =
+                tags.stream()
+                        .map(tag -> questionTagService.createQuestionTag(question, tag))
+                        .collect(Collectors.toList());
+
+        question.getQuestionTags().addAll(questionTags);
+    }
+
+    public void updateQuestion(Long questionId, MemberPrincipal memberPrincipal, QuestionDto.Patch dto) {
+        Question question = questionRepository.getReferenceById(questionId);
+
+        if (question.getMember().getId() != memberPrincipal.getId()) { throw new BusinessLogicException(ExceptionCode.UNAUTHORIZED_MEMBER); }
+
+        Optional.ofNullable(dto.getTitle())
+                .ifPresent(question::setTitle);
+        Optional.ofNullable(dto.getContent())
+                .ifPresent(question::setContent);
+
+        if (dto.getTags() != null) {
+            // 추가할 Tag 목록
+            Set<Tag> addTags = new HashSet<>(tagService.convertToTags(dto.getTags()));
+
+            // 제거할 QuestionTag 목록
+            List<QuestionTag> removeQuestionTags = new ArrayList<>();
+
+            // 제거할 목록과 추가할 목록 갱신
+            question.getQuestionTags()
+                    .forEach(questionTag -> { // 기존에 연결된 tag 들을 확인
+                        Tag oldTag = questionTag.getTag();
+                        // 이미 존재하는 Tag 의 경우 추가할 목록에서 제외
+                        if (addTags.contains(oldTag)) {
+                            addTags.remove(oldTag);
+                        } else {
+                            // 그렇지 않다면 제거할 목록에 추가
+                            removeQuestionTags.add(questionTag);
+                        }
+                    });
+
+            // update 하며 사라진 tag 들과의 연관관계 제거
+            // DB 에서 삭제
+            question.getQuestionTags().removeAll(removeQuestionTags);
+            questionTagService.deleteQuestionTags(removeQuestionTags);
+
+            // 추가할 Tag 목록으로부터 추가할 QuestionTag 목록 생성
+            List<QuestionTag> addQuestionTags =
+                    addTags.stream()
+                            .map(tag -> QuestionTag.of(question, tag))
+                            .collect(Collectors.toList());
+
+            // 추가할 QuestionTag 목록을 Question 에 추가
+            // DB 에 저장
+            addQuestionTags = questionTagService.createQuestionTag(addQuestionTags);
+            question.getQuestionTags().addAll(addQuestionTags);
+        }
+    }
+
+    public void deleteQuestion(Long questionId, MemberPrincipal memberPrincipal) {
+        Question question = questionRepository.getReferenceById(questionId);
+
+        if (question.getMember().getId() != memberPrincipal.getId()) { throw new BusinessLogicException(ExceptionCode.UNAUTHORIZED_MEMBER); }
+        if (question.getStatus() == BasicStatus.DELETED) { throw new BusinessLogicException(ExceptionCode.GONE); }
+
+        question.setStatus(BasicStatus.DELETED);
     }
 }
---
+++ server/src/main/java/com/primenumber/stackoverflow/service/QuestionTagService.java
@@ -0,0 +1,35 @@
+package com.primenumber.stackoverflow.service;
+
+import com.primenumber.stackoverflow.entity.Question;
+import com.primenumber.stackoverflow.entity.QuestionTag;
+import com.primenumber.stackoverflow.entity.Tag;
+import com.primenumber.stackoverflow.repository.QuestionTagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class QuestionTagService {
+    private final QuestionTagRepository questionTagRepository;
+
+    public QuestionTag createQuestionTag(Question question, Tag tag) {
+        return questionTagRepository.save(QuestionTag.of(question, tag));
+    }
+
+    public List<QuestionTag> createQuestionTag(List<QuestionTag> questionTags) {
+        return questionTagRepository.saveAll(questionTags);
+    }
+
+    public void deleteQuestionTag(Long questionTagId) {
+        questionTagRepository.deleteById(questionTagId);
+    }
+
+    public void deleteQuestionTags(List<QuestionTag> questionTags) {
+        questionTagRepository.deleteAll(questionTags);
+    }
+}
