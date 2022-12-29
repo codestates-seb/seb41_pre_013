3c4bf978a3423d21d2fe99ca0524df91763a394b
sGOM <pooh6195@naver.com>
2022-12-29 12:00:00 +0900

    feat: Request 유효성 검사
    Member POST, PATCH 에 대한 유효성 검사 추가
    
    Response 와 Login은 작성은 했으나 적용되지 않음

--- server/src/main/java/com/primenumber/stackoverflow/dto/MemberDto.java
+++ server/src/main/java/com/primenumber/stackoverflow/dto/MemberDto.java
@@ -7,19 +7,26 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
+// TODO : @Pattern 에 message 가 출력되지 않음 -> GlobalExceptionAdvice 가 해당 예외를 ConstraintViolationException 로 받게 해야함
 public class MemberDto {
     @Getter
     @AllArgsConstructor(access = AccessLevel.PRIVATE)
     public static class Post {
-        @NotBlank
+        @Email @Size(max = 50)
         private String email;
         @Setter
-        @NotBlank
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])(?=\\S+$).{8,20}$",
+                message = "비밀번호는 숫자, 알파벳, 특수문자(!@#$%^&*)를 최소 하나 씩 포함해야하며 길이는 8-20 사이여야만 합니다.")
         private String password;
 
+        @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣]{3,20}$",
+                message = "닉네임은 알파벳, 한글, 숫자를 사용할 수 있으며 길이는 3-20 사이여야만 합니다.")
         private String displayName;
 
         public static MemberDto.Post of(String email, String password, String displayName) {
@@ -35,8 +42,12 @@ public class MemberDto {
     @AllArgsConstructor(access = AccessLevel.PRIVATE)
     public static class Patch {
         @Setter
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])(?=\\S+$).{8,20}$",
+                message = "비밀번호는 숫자, 알파벳, 특수문자(!@#$%^&*)를 최소 하나 씩 포함해야하며 길이는 8-20 사이여야만 합니다.")
         private String password;
 
+        @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣]{3,20}$",
+                message = "닉네임은 알파벳, 한글, 숫자를 사용할 수 있으며 길이는 3-20 사이여야만 합니다.")
         private String displayName;
 
         public static MemberDto.Patch of(String password, String displayName) {
@@ -47,8 +58,12 @@ public class MemberDto {
     @Getter
     @AllArgsConstructor(access = AccessLevel.PRIVATE)
     public static class Response {
+        @Positive
         private Long id;
-        private String email;   // 표시할 필요가 없다면 삭제 할 수 있음
+        @Email @Size(max = 50)
+        private String email;
+        @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣]{3,20}$",
+                message = "닉네임은 알파벳, 한글, 숫자를 사용할 수 있으며 길이는 3-20 사이여야만 합니다.")
         private String displayName;
         private MemberStatus status;
         private LocalDateTime createdAt;
