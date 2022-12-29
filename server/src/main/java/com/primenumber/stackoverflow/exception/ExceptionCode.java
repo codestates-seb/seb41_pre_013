899d7d3aba545d14d003f427f5b6d84f2c6098e5
sGOM <pooh6195@naver.com>
2022-12-29 12:25:28 +0900

    feat: 예외 코드 추가
    410 GONE -> 이미 삭제한 리소스를 삭제 요청하는 경우

--- server/src/main/java/com/primenumber/stackoverflow/exception/ExceptionCode.java
+++ server/src/main/java/com/primenumber/stackoverflow/exception/ExceptionCode.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum ExceptionCode {
-    UNAUTHORIZED_MEMBER(403, "해당 요청을 수행할 권한이 없습니다.");
+    UNAUTHORIZED_MEMBER(403, "해당 요청을 수행할 권한이 없습니다."),
+    GONE(410, "이미 삭제된 엔티티입니다.");
 
     private int status;
     private String message;
