26afe8832ce562d8e9171ccb2e8fb8a6d833e674
gidoldory <gidol79@gmail.com>
2023-01-01 17:52:02 +0900

    feat: 상단, 좌측 네비 메뉴 동적로딩 삭제

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
-import React, { Suspense } from 'react';
-import GlobalStyle from './GlobalStyle';
-import React, { Suspense, useState } from 'react';
-import ScrollTop from './components/ScrollTop';
 import { Routes, Route, useLocation } from 'react-router-dom';
+import GlobalStyle from './GlobalStyle';
+import React, { lazy, Suspense, useState } from 'react';
+import ScrollTop from './components/ScrollTop';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Loading from './components/Loading';
 
-const Questions = React.lazy(() => import('./pages/QuestionList'));
-const QuestionDetail = React.lazy(() => import('./pages/QuestionDetail'));
-const LoginPage = React.lazy(() => import('./pages/LoginPage'));
-const SignUpPage = React.lazy(() => import('./pages/SignUpPage'));
-const AskQuestion = React.lazy(() => import('./pages/AskQuestion'));
-const AskQuestionEdit = React.lazy(() => import('./pages/AskQuestionEdit'));
-const AnswerEdit = React.lazy(() => import('./pages/AnswerEdit'));
-const Loading = React.lazy(() => import('./components/Loading'));
+const Questions = lazy(() => import('./pages/QuestionList'));
+const QuestionDetail = lazy(() => import('./pages/QuestionDetail'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const SignUpPage = lazy(() => import('./pages/SignUpPage'));
+const AskQuestion = lazy(() => import('./pages/AskQuestion'));
+const AskQuestionEdit = lazy(() => import('./pages/AskQuestionEdit'));
+const AnswerEdit = lazy(() => import('./pages/AnswerEdit'));
 
 function App() {
-	const [isLogin, setIsLogin] = useState(false);
+	const [isLogin, setIsLogin] = useState(
+		localStorage.getItem('token') !== null
+	);
 
 	const { pathname } = useLocation();
 	const viewFooter = !(
@@ -27,26 +28,31 @@ function App() {
 			<GlobalStyle />
 			<ScrollTop />
 			<div className="app">
-				<Header isLogin={isLogin} setIsLogin={setIsLogin}/>
-				<Routes>
-					<Route exact path="/" element={<Questions />} />
-					<Route path="/questions" element={<Questions />} />
-					<Route path="/questions/:questionId" element={<QuestionDetail />} />
-					<Route path="/login" element={<LoginPage setIsLogin={setIsLogin}/>} />
-					<Route path="/signup" element={<SignUpPage />} />
-					<Route path="/askquestion" element={<AskQuestion />} />
-					<Route
-						path="/questions/:questionId/edit"
-						element={<AskQuestionEdit />}
-					/>
-					<Route
-						path="/questions/:questionId/answers/:answerId/edit"
-						element={<AnswerEdit />}
-					/>
-					<Route path="/tags" element={<Questions />} />
-					<Route path="/users" element={<Questions />} />
-					<Route path="/companies" element={<Questions />} />
-				</Routes>
+				<Header isLogin={isLogin} setIsLogin={setIsLogin} />
+				<Suspense fallback={<Loading />}>
+					<Routes>
+						<Route exact path="/" element={<Questions />} />
+						<Route path="/questions" element={<Questions />} />
+						<Route path="/questions/:questionId" element={<QuestionDetail />} />
+						<Route
+							path="/login"
+							element={<LoginPage setIsLogin={setIsLogin} />}
+						/>
+						<Route path="/signup" element={<SignUpPage />} />
+						<Route path="/askquestion" element={<AskQuestion />} />
+						<Route
+							path="/questions/:questionId/edit"
+							element={<AskQuestionEdit />}
+						/>
+						<Route
+							path="/questions/:questionId/answers/:answerId/edit"
+							element={<AnswerEdit />}
+						/>
+						<Route path="/tags" element={<Questions />} />
+						<Route path="/users" element={<Questions />} />
+						<Route path="/companies" element={<Questions />} />
+					</Routes>
+				</Suspense>
 				{viewFooter && <Footer />}
 			</div>
 		</>
