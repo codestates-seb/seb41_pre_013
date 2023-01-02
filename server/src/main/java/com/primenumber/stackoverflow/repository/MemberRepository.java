14bce517807d0cb9ba033ad5d100b1914f5c518e
Heo SeokJin <pooh6195@naver.com>
2023-01-02 11:57:09 +0900

    feat: `Status` 를 적용한 로직으로 수정 (#55)
    엔티티를 삭제하면, DB에서 삭제하는 것이 아니라
    `status`를 삭제 상태인 것으로 변경하기함
    
    이를 실제 로직에 적용

--- server/src/main/java/com/primenumber/stackoverflow/repository/MemberRepository.java
+++ server/src/main/java/com/primenumber/stackoverflow/repository/MemberRepository.java
@@ -1,10 +1,17 @@
 package com.primenumber.stackoverflow.repository;
 
 import com.primenumber.stackoverflow.entity.Member;
+import com.primenumber.stackoverflow.entity.util.MemberStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(String username);
+    Page<Member> findAllByStatus(Pageable pageable, MemberStatus status);
+
+    Optional<Member> findByEmailAndStatus(String email, MemberStatus status);
+
+    Optional<Member> findByIdAndStatus(Long memberId, MemberStatus status);
 }
