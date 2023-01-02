e895da04393708e086f15f939e676baef12249a3
tami0010 <ggoct101@naver.com>
2023-01-02 11:03:43 +0900

    test: 답변 목록 조회 기능 컨트롤러 테스트

--- server/src/main/java/com/primenumber/stackoverflow/service/MemberService.java
+++ server/src/main/java/com/primenumber/stackoverflow/service/MemberService.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Objects;
 import java.util.Optional;
 
 @RequiredArgsConstructor
